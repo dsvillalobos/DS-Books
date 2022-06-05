6bbef89b5b1db63fff608c62ff21e33bc2d2a095
Sebastian Villalobos <dsvillacorta@gmail.com>
2022-06-05 17:41:27 -0600

    1st Commit
    -Created the Index Page.
    -Created two Categories (Adventure, Fiction).
    -Added four Books to Adventure.

---
+++ JavaScript/Sidenav.js
@@ -0,0 +1,11 @@
+//JavaScript
+
+//Open Sidenav
+function openSidenav() {
+    document.getElementById("Sidenav").style.width = "100%";
+}
+
+//Close Sidenav
+function closeSidenav() {
+    document.getElementById("Sidenav").style.width = "0%";
+}
---
+++ JavaScript/Templates.js
@@ -0,0 +1,48 @@
+//JavaScript
+
+//Header
+class Header extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <header class="Header">
+                <h1 class="Title animate__bounceIn">
+                    <a href="javascript:void(0)" onclick="openSidenav()">
+                        <i class="bi bi-book-fill"></i>
+                        DS Books
+                    </a>
+                </h1>
+            </header>
+        `
+    }
+}
+
+customElements.define("import-header", Header);
+
+//Sidenav
+class Sidenav extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <div class="Sidenav" id="Sidenav">
+                <!-- Close Button -->
+                <a href="javascript:void(0)" class="CloseBTN" onclick="closeSidenav()">
+                    <i class="bi bi-x-lg"></i>
+                </a>
+                <!-- Home -->
+                <a href="./index.html" class="Option">
+                    Home
+                </a>
+                <!-- Categories -->
+                <!-- Adventure -->
+                <a class="Option" href="./Adventure.html">
+                    Adventure
+                </a>
+                <!-- Fiction -->
+                <a class="Option" href="./Fiction.html">
+                    Fiction
+                </a>
+            </div>
+        `
+    }
+}
+
+customElements.define("import-sidenav", Sidenav);
