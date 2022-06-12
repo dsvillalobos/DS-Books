8979a67cb60040c2bf709cd84fd0d09d8078e5de
Sebastian Villalobos <dsvillacorta@gmail.com>
2022-06-11 22:11:02 -0600

    DS Books Version 0.1.8
    -Fixed the Books' links for iOS, Windows and MacOS.
    -Fixed the Books' links for Android

---
+++ JavaScript/Categories/Adventure.js
@@ -0,0 +1,14 @@
+//JavaScript
+
+getMobileOperatingSystem();
+
+function getMobileOperatingSystem() {
+    let UserAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    if (/android/i.test(UserAgent)) {
+        document.getElementById("Treasure-Island").setAttribute("href", "https://drive.google.com/file/d/1wNzji0R7rSPdVp-v-_D81xvcNX_TSfNC/view?usp=sharing");
+        document.getElementById("20000-Leagues-Under-the-Sea").setAttribute("href", "https://drive.google.com/file/d/14-1JckltR7_iewDNl1RLoc1ON-VMezPM/view?usp=sharing");
+        document.getElementById("The-War-of-the-Worlds").setAttribute("href", "https://drive.google.com/file/d/1kPvnnYvoz4CSoZ9KomdQuHvTILw5hg3e/view?usp=sharing");
+        document.getElementById("Around-the-World-in-Eighty-Days").setAttribute("href", "https://drive.google.com/file/d/1xhrKhcNQ7zNcRHb0wWKtLwdQFmXMN15W/view?usp=sharing");
+    }
+}
---
+++ JavaScript/Categories/Fiction.js
@@ -0,0 +1,14 @@
+//JavaScript
+
+getMobileOperatingSystem();
+
+function getMobileOperatingSystem() {
+    let UserAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    if (/android/i.test(UserAgent)) {
+        document.getElementById("1984").setAttribute("href", "https://drive.google.com/file/d/12xPWzn2ctLUAY_a8ZivagmDsdc7rWo1V/view?usp=sharing");
+        document.getElementById("The-Invisible-Man").setAttribute("href", "https://drive.google.com/file/d/1kc8EnMKMEQQ8ARE74NHl3USDGOkVTLj6/view?usp=sharing");
+        document.getElementById("The-Book-Thief").setAttribute("href", "https://drive.google.com/file/d/1oL3aueaLefJoKf1LL6gH0cOOs0AarM9i/view?usp=sharing");
+        document.getElementById("Fahrenheit-451").setAttribute("href", "https://drive.google.com/file/d/1kBUnFc-CTxgHODZMZ3f6C__PUVczpWxC/view?usp=sharing");
+    }
+}
