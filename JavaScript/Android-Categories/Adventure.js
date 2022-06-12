7b68b82ce01c3d19f0af5836a0c0522ce2fadc58
Sebastian Villalobos <dsvillacorta@gmail.com>
2022-06-12 15:49:28 -0600

    DS Books Version 0.1.8
    -Added New Books in the Adventure and Fiction Categories.
    -Fixed some Bugs.

--- JavaScript/Android-Categories/Adventure.js
+++ JavaScript/Android-Categories/Adventure.js
@@ -10,5 +10,7 @@ function getMobileOperatingSystem() {
         document.getElementById("20000-Leagues-Under-the-Sea").setAttribute("href", "https://drive.google.com/file/d/14-1JckltR7_iewDNl1RLoc1ON-VMezPM/view?usp=sharing");
         document.getElementById("The-War-of-the-Worlds").setAttribute("href", "https://drive.google.com/file/d/1kPvnnYvoz4CSoZ9KomdQuHvTILw5hg3e/view?usp=sharing");
         document.getElementById("Around-the-World-in-Eighty-Days").setAttribute("href", "https://drive.google.com/file/d/1xhrKhcNQ7zNcRHb0wWKtLwdQFmXMN15W/view?usp=sharing");
+        document.getElementById("The-Alchemist").setAttribute("href", "https://drive.google.com/file/d/1jbHTVur345RfHEPjEB7HoygPhbcM_upW/view?usp=sharing");
+        document.getElementById("The-Martian").setAttribute("href", "https://drive.google.com/file/d/17XaVB09_uyvTF4NmQqBXKENJXpc9E9fm/view?usp=sharing");
     }
 }
--- JavaScript/Android-Categories/Fiction.js
+++ JavaScript/Android-Categories/Fiction.js
@@ -10,5 +10,7 @@ function getMobileOperatingSystem() {
         document.getElementById("The-Invisible-Man").setAttribute("href", "https://drive.google.com/file/d/1kc8EnMKMEQQ8ARE74NHl3USDGOkVTLj6/view?usp=sharing");
         document.getElementById("The-Book-Thief").setAttribute("href", "https://drive.google.com/file/d/1oL3aueaLefJoKf1LL6gH0cOOs0AarM9i/view?usp=sharing");
         document.getElementById("Fahrenheit-451").setAttribute("href", "https://drive.google.com/file/d/1kBUnFc-CTxgHODZMZ3f6C__PUVczpWxC/view?usp=sharing");
+        document.getElementById("The-Martian").setAttribute("href", "https://drive.google.com/file/d/17XaVB09_uyvTF4NmQqBXKENJXpc9E9fm/view?usp=sharing");
+        document.getElementById("Pride-and-Prejudice").setAttribute("href", "https://drive.google.com/file/d/1K5j4om7pMKors0komr9rsx9feo_FFuAg/view?usp=sharing");
     }
 }
