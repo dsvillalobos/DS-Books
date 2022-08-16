b8af1d3afbd8aac09b7f9b5f6559ac3289d51097
Sebastian Villalobos <dsvillacorta@gmail.com>
2022-08-16 00:12:39 -0600

    DS Books Version 0.2.7
    Release Notes 0.2.7:
    
    -Bug Fixes.
    -New "About" Page.
    -New "Offline" Page.
    -Cross Platform App Support.

--- JavaScript/SW-Register.js
+++ JavaScript/SW-Register.js
@@ -3,7 +3,7 @@
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("Service-Worker.js").then(registration => {
         console.log("Service Worker Registered :)");
-        console.log(registration);
+        //console.log(registration);
     }).catch(error => {
         console.log("Service Worker Registration Failed :(");
         console.log(error);
