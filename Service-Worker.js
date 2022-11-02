eb5ffa7fb11b4341f04de0774352b6cb557bcb34
Sebastian Villalobos <dsvillacorta@gmail.com>
2022-11-01 20:27:46 -0600

    DS Books Version 0.5.9
    Release Notes:
    
    - Bug Fixes.
    - App User Interface Enhancement.
    - New "Trending" Section.
    - New "New Books" Section.
    - Code Formatted.

--- Service-Worker.js
+++ Service-Worker.js
@@ -39,11 +39,11 @@ self.addEventListener("install", (e) => {
         "/Book-Cover/Tech/Steve-Jobs-Book-Cover.webp",
 
         //Cache the Trending Books
-        "/Book-Cover/Adventure/The-Martian-Book-Cover.webp", //From the Adventure Category
+        //The Martian is already cached
         //11.22.63 is already cached
         //The Book Thief is alredy cached
-        //1984 is alredy cached
-        "/Book-Cover/Fiction/Pride-and-Prejudice-Book-Cover.webp", //From the Fiction Category
+        //1984 is already cached
+        //Pride and Prejudice is already cached
 
         //Cache the Top Categories' Covers
         "/Images/Category-IMG/Adventure.webp",
@@ -58,30 +58,38 @@ self.addEventListener("install", (e) => {
         "/JavaScript/Category/Thriller.js",
         "/JavaScript/Category/Tech.js",
 
-        //Cache the first 4 Book Covers of each top category
+        //Cache the first 6 Book Covers of each top category
         //Adventure
         "/Book-Cover/Adventure/Treasure-Island-Book-Cover.webp",
         "/Book-Cover/Adventure/20000-Leagues-Under-The-Sea-Book-Cover.webp",
         "/Book-Cover/Adventure/The-War-of-the-Worlds-Book-Cover.webp",
         "/Book-Cover/Adventure/Around-the-World-in-Eighty-Days-Book-Cover.webp",
+        "/Book-Cover/Adventure/The-Alchemist-Book-Cover.webp",
+        "/Book-Cover/Adventure/The-Martian-Book-Cover.webp",
 
         //Fiction
         "/Book-Cover/Fiction/1984-Book-Cover.webp",
         "/Book-Cover/Fiction/The-Invisible-Man-Book-Cover.webp",
         "/Book-Cover/Fiction/The-Book-Thief-Book-Cover.webp",
         "/Book-Cover/Fiction/Fahrenheit-451-Book-Cover.webp",
+        "/Book-Cover/Fiction/The-Martian-Book-Cover.webp",
+        "/Book-Cover/Fiction/Pride-and-Prejudice-Book-Cover.webp",
 
         //Romance
         "/Book-Cover/Romance/The-Fault-in-our-Stars-Book-Cover.webp",
         "/Book-Cover/Romance/Book-Lovers-Book-Cover.webp",
         "/Book-Cover/Romance/Pride-and-Prejudice-Book-Cover.webp",
         "/Book-Cover/Romance/Jane-Eyre-Book-Cover.webp",
+        "/Book-Cover/Romance/Emma-Book-Cover.webp",
+        "/Book-Cover/Romance/The-Phantom-of-the-Opera-Book-Cover.webp",
 
         //Thriller
         "/Book-Cover/Thriller/11.22.63-Book-Cover.webp",
         "/Book-Cover/Thriller/Crime-and-Punishment-Book-Cover.webp",
         "/Book-Cover/Thriller/Dark-Matter-Book-Cover.webp",
         "/Book-Cover/Thriller/Misery-Book-Cover.webp",
+        "/Book-Cover/Thriller/Strangers-on-a-Train-Book-Cover.webp",
+        "/Book-Cover/Thriller/Helter-Skelter-Book-Cover.webp",
       ]);
     })
   );
